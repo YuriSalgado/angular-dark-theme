aea39da23698a4485c0b8a8070d0302dbe3ceab6
Yuri Salgado <yuri.salgado@fretebras.com.br>
2022-03-30 19:55:05 -0300

    feat(toogle-theme): create css vars, set/get enum themes and click action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+enum THEMES { "light", "dark"};
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angular-dark-theme';
+  themes = THEMES;
+
+  setThemeClass(theme:THEMES) {
+    const bodyTag = document.body;
+
+    for (const theme in this.themes) {
+      bodyTag.classList.remove(theme);      
+    }
+    bodyTag.classList.add(THEMES[theme]);
+  }
 }
